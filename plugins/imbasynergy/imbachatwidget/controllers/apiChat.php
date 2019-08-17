187bb004e68cfb75cd903dc94d90cbf91d1d1d82
victor <victor@victor-To-be-filled-by-O-E-M>
2019-08-18 00:54:24 +1000

    Убрал старую тему Поправил апи. (надо протестировать)

--- plugins/imbasynergy/imbachatwidget/controllers/apiChat.php
+++ plugins/imbasynergy/imbachatwidget/controllers/apiChat.php
@@ -1,10 +1,8 @@
 <?php namespace ImbaSynergy\imbachatwidget\Controllers;
 
 use Backend\Classes\Controller;
-use BackendMenu;
 use Auth;
 use RainLab\User\Models\User;
-use ImbaSynergy\imbachatwidget\Components\imbaChat;
 use ImbaSynergy\imbachatwidget\Models\Settings;
 
 class apiChat extends Controller
@@ -16,53 +14,99 @@ class apiChat extends Controller
         parent::__construct();
     }
 
-    public function getUser($str_ids){
+    /**
+     * API authorization check
+     */
+    protected function testAuthOrDie()
+    {
         $login = Settings::get('dev_login');
         $password = Settings::get('dev_password');
 
-        if(!isset($_SERVER['PHP_AUTH_USER']) || ($_SERVER['PHP_AUTH_PW']!=$password) || strtolower($_SERVER['PHP_AUTH_USER'])!=$login)
+        if(!isset($_SERVER['PHP_AUTH_USER'])
+                || ($_SERVER['PHP_AUTH_PW']!=$password) 
+                || strtolower($_SERVER['PHP_AUTH_USER'])!=$login)
         {
             header('WWW-Authenticate: Basic realm="Backend"');
             header('HTTP/1.0 401 Unauthorized');
             echo 'Authenticate required!';
             die();
         }
-
+    }
+    
+    /**
+     * API chat integration. Provides information about the user by his id
+     * 
+     * @param string $str_ids
+     * @return array
+     */
+    public function getUser($str_ids){
+        
+        $this->testAuthOrDie();
         $ids = explode(",", $str_ids);
-        $users = array();
-        foreach ($ids as $id){
-            $user_m = @User::where('id', $id)->get()[0];
-            if(!$user_m)
-                continue;
-            $user = array();
+        $users = [];
+        
+        $res = User::whereIn('id', $ids)->get(); 
+        foreach ($res as $user_m){ 
+            $user = [];
             $user['name'] = $user_m->name;
-            $user['user_id'] =  $user_m->id;
+            $user['user_id'] =  $user_m->id; 
             $users[] = $user;
         }
-        return json_encode($users);
+        
+        return $users;
     }
+    
+    /**
+     * API chat integration. Provides information about the user by his login and password
+     * 
+     * @param string $str_ids
+     * @return array
+     */
     public function authUser(){
-        $login = Settings::get('dev_login');
-        $password = Settings::get('dev_password');
-
-        if(!isset($_SERVER['PHP_AUTH_USER']) || ($_SERVER['PHP_AUTH_PW']!=$password) || strtolower($_SERVER['PHP_AUTH_USER'])!=$login)
-        {
-            header('WWW-Authenticate: Basic realm="Backend"');
-            header('HTTP/1.0 401 Unauthorized');
-            echo 'Authenticate required!';
-            die();
-        }
+        
+        $this->testAuthOrDie();
+        
         try{
             $user_m = Auth::authenticate([
                 'login' => post('login'),
                 'password' => post('password')
             ]);
         }catch (\October\Rain\Auth\AuthException $e){
-            return 'Please enter a correct username and password. Note that both fields may be case-sensitive.';
+            return [
+                "code" => 403,
+                "error" => 'Please enter a correct username and password. Note that both fields may be case-sensitive.'
+            ];
         }
+        
         $user = array();
         $user['name'] = $user_m['name'];
         $user['user_id'] =  $user_m['id'];
         return $user;
+    }
+    
+    
+    /**
+     * API chat integration. 
+     * Provides information about the users by search string 
+     * @return array
+     */
+    public function search(){
+        // ..
+        $this->testAuthOrDie(); 
+        return [];
+    }
+    
+    /**
+     * API chat integration. 
+     * Provides information about api version
+     * @return array
+     */
+    public function getApiVersion(){
+        // ..
+        $this->testAuthOrDie();
+        return [
+            "version" => 1.0,
+            "type" => "OctoberCMS plugin"
+        ];
     }
 }
--- plugins/imbasynergy/imbachatwidget/routes.php
+++ plugins/imbasynergy/imbachatwidget/routes.php
@@ -1,7 +1,9 @@
 <?php
 
-Route::get('imbachat/api/v1/users/{ids}', 'ImbaSynergy\imbachatwidget\Controllers\apiChat@getuser');
 Route::post('imbachat/api/v1/auth_users', 'ImbaSynergy\imbachatwidget\Controllers\apiChat@authUser');
+Route::get('imbachat/api/v1/users/{ids}', 'ImbaSynergy\imbachatwidget\Controllers\apiChat@getuser');
+Route::get('imbachat/api/v1', 'ImbaSynergy\imbachatwidget\Controllers\apiChat@getApiVersion');
+Route::get('imbachat/api/v1/search', 'ImbaSynergy\imbachatwidget\Controllers\apiChat@search');
+ 
 Route::get('imbachat/jwt', 'ImbaSynergy\imbachatwidget\Components\ImbaChat@getJWT');
-
-?>
+ 
